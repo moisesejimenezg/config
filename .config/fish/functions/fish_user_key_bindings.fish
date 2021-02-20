415196068462fd5121f39cf7bcf36e4c2679ded3
Moises Jimenez <moisesejimenezg@gmail.com>
2021-02-20 21:15:34 +0100

    fish

---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,9 @@
+function fish_user_key_bindings
+  fzf_key_bindings
+  fish_vi_key_bindings
+  for mode in insert default visual
+    bind -M $mode \cf forward-char
+  end
+
+  bind -M insert \cb fzf-git-branch
+end
