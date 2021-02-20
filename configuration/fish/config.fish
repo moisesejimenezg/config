7117d9cf01774c3dcb61d7fe2eca1824ccdee2b0
Moises Jimenez <moisesejimenezg@outlook.com>
2021-02-20 20:56:07 +0100

    Update fish config

--- configuration/fish/config.fish
+++ configuration/fish/config.fish
@@ -1,54 +1,11 @@
-# Path to Oh My Fish install.
-set -q XDG_DATA_HOME
-  and set -gx OMF_PATH "$XDG_DATA_HOME/omf"
-  or set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Load Oh My Fish configuration.
-source $OMF_PATH/init.fish
-
-#set -g theme_color_scheme zenburn
-
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
+if status is-interactive
+and not set -q TMUX
+    exec tmux
 end
 
-# bmwrpp
-if test -e /etc/bmwrpp-bootstrap/zshrc
-  bass source /etc/bmwrpp-bootstrap/bashrc
-else
-  echo "bmwrpp-bootstrap not installed, please remove BMWRPP section from /home/jimenez/.zshrc"
-end
-
-# ros
-bass source /opt/ros/melodic/setup.bash
-source /opt/ros/melodic/share/rosbash/rosfish
-
-# Environment variables
-set -gx PATH /usr/lib/ccache $PATH
-set -gx ROS_IP localhost
-set -gx ROS_HOSTNAME localhost
-set -gx ROS_MASTER_URI http://$ROS_IP:11311
-set -gx FZF_DEFAULT_COMMAND 'rg --color=auto --files --no-ignore --hidden --follow --glob "!.git/*" --glob "!bazel-*/*"'
-set -gx RIPGREP_CONFIG_PATH ~/.ripgreprc
-
-# Add GO to PATH
-set -x -U GOPATH $HOME/go
-set -gx PATH $HOME/go/bin $PATH
-
-# Add directory for fish functions
-set -gx PATH $HOME/.config/fish/functions $PATH
-
-# Add autostart to fish
-set -gx PATH $HOME/.config/autostart $PATH
-
-# tool aliases
-alias hub=hub
-alias cat=bat
-alias ls=exa
-
-# git aliases
+# git abbreviations
 abbr gau git add -u
+abbr gaup git add -up
 abbr gbd git branch -d
 abbr gbD git branch -D
 abbr gcam git commit --amend
@@ -59,18 +16,19 @@ abbr gstap git stash pop
 abbr gstal git stash list
 abbr gstad git stash drop
 
-# utility aliases
+# utility abbreviations
 # search
 abbr fft fish_find_text
 
-set -gx bazel_cache $HOME/.ccache
-# bazel aliases
-abbr bba bazel build --config=adp
-abbr bta bazel test --config=adp
-abbr bbh bazel build --sandbox_writable_path ~/.ccache --local_test_jobs=1 --define=target_platform=hPAD --cxxopt=-Wno-error=deprecated-declarations --cache_test_results=no --test_output=streamed
-abbr bth bazel test --sandbox_writable_path ~/.ccache --local_test_jobs=1 --define=target_platform=hPAD --cxxopt=-Wno-error=deprecated-declarations --cache_test_results=no --test_output=streamed
+#aliases
+alias cat=bat
 
 # theme
 set -g theme_color_scheme dracula
 
-#task
+set -x FZF_DEFAULT_COMMAND 'rg --files'
+set -x FZF_CTRL_T_COMMAND $FZF_DEFAULT_COMMAND
+set -x FZF_CTRL_T_OPTS '--preview \'cat {}\''
+set -x FZF_ALT_C_COMMAND 'rg --files --null | xargs -0 dirname | uniq'
+
+set -x XDG_CONFIG_HOME '/home/mjimenez/.config'
--- configuration/fish/functions/fish_user_key_bindings.fish
+++ configuration/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,6 @@
 function fish_user_key_bindings
+  fzf_key_bindings
+  fish_vi_key_bindings
   for mode in insert default visual
     bind -M $mode \cf forward-char
   end
@@ -6,4 +8,3 @@ function fish_user_key_bindings
   bind -M insert \cn fzf-bazel-target
   bind -M insert \cb fzf-git-branch
 end
-
