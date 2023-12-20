9fab5a9917a48d2e13ec819bd924c012c49d7b08
Moises Jimenez <moises.jimenez@apex.ai>
2023-12-20 12:06:18 +0100

    Extract environment variables into different file

---
+++ .config/zsh/environment_variables.zsh
@@ -0,0 +1,7 @@
+# config files
+export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
+export TMUX_CONF=$HOME/.tmux.conf
+
+# options
+export FZF_DEFAULT_COMMAND="rg --files --ignore-files"
+export FZF_DEFAULT_OPTS="--reverse --preview 'batcat --color=always {}'"
