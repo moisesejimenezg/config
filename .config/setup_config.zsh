af3bdd7021a4aea5c563410dccf396c4cae6c49d
Moises Jimenez <moises.jimenez@apex.ai>
2023-12-20 12:14:30 +0100

    Fix tmux configuration

--- .config/setup_config.zsh
+++ .config/setup_config.zsh
@@ -2,3 +2,4 @@
 
 ln -s $HOME/.config/zsh/zshrc $HOME/.zshrc
 ln -s $HOME/.config/starship/starship.toml $HOME/.config/starship.toml
+ln -s $HOME/.config/tmux/tmux.conf $HOME/.tmux.conf
--- .config/zsh/environment_variables.zsh
+++ .config/zsh/environment_variables.zsh
@@ -1,6 +1,5 @@
 # config files
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
-export TMUX_CONF=$HOME/.tmux.conf
 
 # options
 export FZF_DEFAULT_COMMAND="rg --files --ignore-files"
