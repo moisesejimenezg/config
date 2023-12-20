ae60571b2363370e005a408c414e3a4b7d4ef0c6
Moises Jimenez <moises.jimenez@apex.ai>
2023-12-20 12:24:42 +0100

    Improve fzf customization

--- .config/zsh/environment_variables.zsh
+++ .config/zsh/environment_variables.zsh
@@ -1,6 +1,15 @@
 # config files
 export STARSHIP_CONFIG=$HOME/.config/starship/starship.toml
 
-# options
-export FZF_DEFAULT_COMMAND="rg --files --ignore-files"
+# Options
+
+# fd
+export FD_OPTIONS="--follow --exclude .git"
+
+# fzf
+export GIT_LS_COMMAND="git ls-files cached --others --exclude-standard"
+export FZF_DEFAULT_COMMAND="$GIT_LS_COMMAND | fd --type f --type l $FD_OPTIONS"
+
+export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
+export FZF_ALT_C_COMMAND="fd --type d $FD_OPTIONS"
 export FZF_DEFAULT_OPTS="--reverse --preview 'batcat --color=always {}'"
