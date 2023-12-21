b77bf6c1b1ac807a2a4cb61a1228d233cdc3d5eb
Moises Jimenez <moises.jimenez@apex.ai>
2023-12-21 14:27:30 +0100

    Update neovim config

---
+++ .config/nvim/lua/telescope_config.lua
@@ -0,0 +1,12 @@
+local actions = require("telescope.actions")
+require("telescope").setup({
+	pickers = {
+		buffers = {
+			mappings = {
+				n = {
+					["<c-D>"] = actions.delete_buffer + actions.move_to_top,
+				},
+			},
+		},
+	},
+})
