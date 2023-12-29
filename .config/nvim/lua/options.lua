c0ec4e110dab81a072867ac01c63644306ada3fe
Moises Jimenez <moises.jimenez@apex.ai>
2023-12-29 10:28:36 +0100

    Change nvim theme

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -17,4 +17,20 @@ vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 
-vim.cmd("colorscheme nord")
+vim.cmd("colorscheme OceanicNext")
+--vim.cmd("colorscheme afterglow")
+--vim.cmd("colorscheme alduin")
+--vim.cmd("colorscheme apprentice")
+--vim.cmd("colorscheme archery")
+--vim.cmd("colorscheme atom")
+--vim.cmd("colorscheme deep-space")
+--vim.cmd("colorscheme desert")
+--vim.cmd("colorscheme dogrun")
+--vim.cmd("colorscheme gruvbox")
+--vim.cmd("colorscheme nord")
+--vim.cmd("colorscheme oceanic_material")
+--vim.cmd("colorscheme snow")
+--vim.cmd("colorscheme snow")
+--vim.cmd("colorscheme solarized8_low")
+--vim.cmd("colorscheme sonokai")
+--vim.cmd("colorscheme wombat256mod")
