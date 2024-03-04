1cc7acd94d32dbbd6e693fdf055b14f62f457301
Moises Jimenez <moises.jimenez@apex.ai>
2024-03-04 10:19:32 +0100

    Fix formatter config

--- .config/nvim/lua/config/formatter/config.lua
+++ .config/nvim/lua/config/formatter/config.lua
@@ -1,46 +1,53 @@
 -- Provides the Format and FormatWrite commands
 require("formatter").setup({
-	logging = true,
-	filetype = {
-		c = {
-			function()
-				local uncrustify = require("formatter.filetypes.c").uncrustify()
-
-				local code_style = vim.fn.findfile(
-					"ament_code_style.cfg",
-					"/home/moises.jimenez/ade-home/gc/apex_ws/src/tools/ament/**"
-				)
-				if code_style and code_style:len() > 0 then
-					table.insert(uncrustify["args"], 1, code_style)
-					table.insert(uncrustify["args"], 1, "-c")
-				end
-				return uncrustify
-			end,
-		},
-		cpp = {
-			function()
-				local uncrustify = require("formatter.filetypes.cpp").uncrustify()
-
-				local code_style = vim.fn.findfile(
-					"ament_code_style.cfg",
-					"/home/moises.jimenez/ade-home/gc/apex_ws/src/tools/ament/**"
-				)
-				if code_style and code_style:len() > 0 then
-					table.insert(uncrustify["args"], 1, code_style)
-					table.insert(uncrustify["args"], 1, "-c")
-				end
-				return uncrustify
-			end,
-		},
-		python = {
-			--require("formatter.filetypes.python").autopep8,
-			--require("formatter.filetypes.python").isort,
-			--require("formatter.filetypes.python").black,
-			require("formatter.filetypes.python").flake8,
-		},
-		lua = {
-			require("formatter.filetypes.lua").stylua,
-		},
-	},
+    logging = true,
+    filetype = {
+        c = {
+            function()
+                local code_style = vim.fn.findfile(
+                    "ament_code_style.cfg",
+                    "/home/moises.jimenez/ade-home/gc/apex_ws/src/tools/ament/**"
+                )
+                local uncrustify = require("formatter.filetypes.c").uncrustify()
+                table.insert(uncrustify["args"], "-c")
+                table.insert(uncrustify["args"], code_style)
+                return uncrustify
+            end,
+        },
+        cpp = {
+            function()
+                local utils = require("formatter.util")
+                if string.find(utils.get_current_buffer_file_path(), "apex_customer") then
+                    return nil
+                end
+                local code_style = vim.fn.findfile(
+                    "ament_code_style.cfg",
+                    "/home/moises.jimenez/ade-home/gc/apex_ws/src/tools/ament/**"
+                )
+                local uncrustify = require("formatter.filetypes.cpp").uncrustify()
+                table.insert(uncrustify["args"], "-c")
+                table.insert(uncrustify["args"], code_style)
+                return uncrustify
+            end,
+        },
+        python = {
+            --require("formatter.filetypes.python").autopep8,
+            --require("formatter.filetypes.python").isort,
+            --require("formatter.filetypes.python").black,
+            require("formatter.filetypes.python").flake8,
+        },
+        lua = {
+            function()
+                local luastyle = vim.fn.findfile("stylua.toml", "/home/moises.jimenez/formatting/**")
+                return {
+                    exe = "stylua",
+                    args = {
+                        "-f",
+                        luastyle,
+                    },
+                }
+            end,
+        },
+    },
 })
 vim.api.nvim_create_autocmd("BufWritePost", { command = "FormatWrite" })
